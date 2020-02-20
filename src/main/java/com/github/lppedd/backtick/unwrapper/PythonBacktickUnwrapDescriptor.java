c39f9ce715a9ab3d6b6d062c4e554f7db9bda639
Edoardo Luppi <lp.edoardo@gmail.com>
2020-02-20 21:43:03 +0100

    feat: support Code > Unwrap for Python

---
+++ src/main/java/com/github/lppedd/backtick/unwrapper/PythonBacktickUnwrapDescriptor.java
@@ -0,0 +1,35 @@
+package com.github.lppedd.backtick.unwrapper;
+
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.codeInsight.unwrap.Unwrapper;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.python.refactoring.unwrap.PyUnwrapDescriptor;
+
+/**
+ * @author Edoardo Luppi
+ */
+class PythonBacktickUnwrapDescriptor extends PyUnwrapDescriptor {
+  @NotNull
+  @Override
+  public List<Pair<PsiElement, Unwrapper>> collectUnwrappers(
+      @NotNull final Project project,
+      @NotNull final Editor editor,
+      @NotNull final PsiFile file) {
+    final var backtickUnwrappers = BacktickUnwrapperCollector.collectUnwrappers(editor, file);
+    final var originalUnwrappers = super.collectUnwrappers(project, editor, file);
+    return ContainerUtil.concat(backtickUnwrappers, originalUnwrappers);
+  }
+
+  @Override
+  public boolean showOptionsDialog() {
+    return true;
+  }
+}
